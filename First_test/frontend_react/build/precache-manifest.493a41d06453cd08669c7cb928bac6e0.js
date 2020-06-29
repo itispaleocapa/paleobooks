9b785fd189d3139e5c4ade4be5aeedb37f9f980f
Adriano Rampoldi <adriano.rampoldi@itispaleocapa.it>
2020-06-29 11:26:18 +0200

    compilazione frontend_react
    - aggiunta file frontend_react/.gitignore per non versionare frontend_react/node_modules
    - build frontend_react

---
+++ First_test/frontend_react/build/precache-manifest.493a41d06453cd08669c7cb928bac6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2cf6f04042ec99b7b3d5f4fa8f5c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6004bf040c632b5bb4",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3e3721d3f3584ce20379",
+    "url": "/static/js/2.c2c3af86.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c2c3af86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6004bf040c632b5bb4",
+    "url": "/static/js/main.bd2ce00a.chunk.js"
+  },
+  {
+    "revision": "605e7409e7213c4551c7",
+    "url": "/static/js/runtime-main.cd57d020.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
