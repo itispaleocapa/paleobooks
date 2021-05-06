556534476b786b2ce16831eb93228186287ad87b
Cristian Livella <cristian@cristianlivella.com>
2021-05-06 22:31:55 +0200

    💩 Test build backend

--- First_test/backend/app/Http/Controllers/AppController.php
+++ First_test/backend/app/Http/Controllers/AppController.php
@@ -11,15 +11,15 @@ use App\Models\Demand;
 class AppController extends Controller {
     public function getInfo(Request $request) {
         $user = User::find($request->auth->id);
-        
+
         $supplies = Supply::count();
         $demands = Demand::count();
         $user_supplies = Supply::where('user_id', $user->id)->count();
         $user_demands = Demand::where('user_id', $user->id)->count();
 
         return response()->json([
-            'supplies' => $supplies,
-            'demands' => $demands,
+            'supplies' => $supplies * 10,
+            'demands' => 123456,
             'user_supplies' => $user_supplies,
             'user_demands' => $user_demands,
         ], 200);
