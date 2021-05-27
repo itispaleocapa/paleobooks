acb30223351cc1d665cc59592d49ca8f0d32ce75
Cristian Livella <cristian@cristianlivella.com>
2021-05-27 23:37:14 +0200

    💚 Automatic execute migrations on backend update

--- First_test/backend/app/Http/Controllers/DatabaseController.php
+++ First_test/backend/app/Http/Controllers/DatabaseController.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\Artisan;
 
 class DatabaseController extends Controller {
     public function update(Request $request){
+        $token = $request->input('token');
+
+        if (strcmp($token, env('MIGRATION_TOKEN')) !== 0) {
+            return response()->json([
+                'error' => 'Token is invalid.'
+            ], 401);
+        }
+
         Artisan::call('migrate', array('--path' => 'database/migrations', '--force' => true));
         return '<h1>Database updated</h1>';
     }
