0c918f5a124bc2beca3533b8a0a812bc9de81baf
Cristian Livella <cristian@cristianlivella.com>
2021-06-02 16:32:23 +0200

    🔒️ Notice the user that there is something wrong with the SSL cert

--- Tools/AmazonScraper/functions.php
+++ Tools/AmazonScraper/functions.php
@@ -7,7 +7,10 @@
  * This script is part of the PALEObooks scraping tool.
 */
 
+$ignoreSslErrors = false;
+
 function getJson($url, $attempt = 1) {
+    global $ignoreSslErrors;
     sleep(rand(1, 3));
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -16,12 +19,15 @@ function getJson($url, $attempt = 1) {
     curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
     curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, !$ignoreSslErrors);
 
     $result = curl_exec($ch);
 
     if (curl_errno($ch) || curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200 || json_decode($result) === null) {
         logMessage('API request failed, attempt ' . (($attempt - 1) % 3 + 1) . ' of 3.');
+        if (curl_errno($ch) === 60) {
+            askCertificateCheck();
+        }
         if ($attempt % 3 === 0) {
             $retry = askNewIp($attempt > 3);
             if (!$retry) {
@@ -52,4 +58,11 @@ function askNewIp($suggestSkip = false) {
         return false;
     }
     return true;
+}
+
+function askCertificateCheck() {
+    global $ignoreSslErrors;
+    logMessage('There seems to be an issue with the SSL certificate of '. ALS_HOST . '. Please check that it is valid, and press enter to continue anyway.', false);
+    readline();
+    $ignoreSslErrors = true;
 }
