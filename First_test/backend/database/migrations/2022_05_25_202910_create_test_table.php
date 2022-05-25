aba4655f1dced7f12f143b8cb43ce7312005687b
Cristian Livella <cristian@cristianlivella.com>
2022-05-25 22:34:07 +0200

    🗃️ Test migration

--- First_test/backend/database/migrations/2022_05_25_202910_create_test_table.php
+++ First_test/backend/database/migrations/2022_05_25_202910_create_test_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Info extends Migration
+class CreateTestTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class Info extends Migration
      */
     public function up()
     {
-        Schema::table('supplies', function (Blueprint $table) {
-            //
+        Schema::create('test', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -25,8 +26,6 @@ class Info extends Migration
      */
     public function down()
     {
-        Schema::table('supplies', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('test');
     }
 }
